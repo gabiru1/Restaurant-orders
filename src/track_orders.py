ed0e07c0ba5f554c04a23cd1a2875f9236d34b0b
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-07-25 22:15:21 -0300

    req2 +-

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,19 +1,56 @@
 class TrackOrders:
     # aqui deve expor a quantidade de estoque
+    def __init__(self):
+            self.data = []
+
     def __len__(self):
-        pass
+        return len(self.data)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.data.append({
+            'cliente': customer,
+            'pedido': order,
+            'dia': day
+        })
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        foods = {}
+        info_cliente = [
+            info for info in self.data if info['cliente'] == customer
+            ]
+
+        for info in info_cliente:
+            if info['pedido'] not in foods:
+                foods[info['pedido']] = 1
+            else:
+                foods[info['pedido']] += 1
+
+        return max(foods, key=foods.get)
+
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        menus = set()
+        custumer_orders = set()
+
+        for info in self.data:
+            if info['cliente'] == customer:
+                custumer_orders.add(info['pedido'])
+            menus.add(info['pedido'])
+        never_ordered = menus.difference(custumer_orders)
+
+        return never_ordered
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        custumer_days = set()
+        days = set()
+
+        for info in self.data:
+            if info['cliente'] == customer:
+                custumer_days.add(info['dia'])
+            days.add(info['dia'])
+        days_never_visited = days.difference(custumer_days)
+
+        return days_never_visited
 
     def get_busiest_day(self):
         pass
